cd23cbe2b1bfc7c002ea940e5ae13d4178d2cb7e
alexsanjoseph <alexsanjoseph@gmail.com>
2022-01-30 23:14:29 +0530

    Removed empty row

--- src/components/grid/Grid.tsx
+++ src/components/grid/Grid.tsx
@@ -1,6 +1,5 @@
 import { CompletedRow } from './CompletedRow'
 import { CurrentRow } from './CurrentRow'
-import { EmptyRow } from './EmptyRow'
 
 type Props = {
   guesses: string[]
@@ -9,18 +8,12 @@ type Props = {
 }
 
 export const Grid = ({ guesses, currentGuess, solution }: Props) => {
-  // const empties = guesses.length < 10 ? Array.from(Array(10 - guesses.length)) : []
-  const empties = Array.from(Array(1))
-
   return (
     <div className="pb-6">
       {guesses.map((guess, i) => (
         <CompletedRow key={i} guess={guess} solution={solution} />
       ))}
       {guesses.length < 10 && <CurrentRow guess={currentGuess} />}
-      {empties.map((_, i) => (
-        <EmptyRow key={i} />
-      ))}
     </div>
   )
 }
